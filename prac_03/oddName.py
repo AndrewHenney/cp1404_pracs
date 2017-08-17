3ddd9a670e92a11eb283a4942dff69e6e412a9b1
jc425395 <andrew.henney@my.jcu.edu.au>
2017-08-17 10:28:23 +1000

    Add functions for get and print name

--- prac_03/oddName.py
+++ prac_03/oddName.py
@@ -1,13 +1,26 @@
-name = input("What is your name: ")
-length = len(name)
-while length<1:
-    print('that is not a valid name')
+
+def main():
+    name = get_name()
+    frequency = 2
+    name_print(name,frequency)
+
+
+def name_print(name,frequency):
+    count=1
+    for char in name:
+        if count % frequency == False :
+            print(char)
+        count += 1
+
+
+
+
+def get_name():
     name = input("What is your name: ")
-    length=len(name)
-counter = 1
+    length = len(name)
+    while length < 1:
+        name = input("What is your name: ")
+        length = len(name)
+    return name
 
-for char in name :
-    if counter % 2:
-        print(char)
-
-    counter += 1
+main()
