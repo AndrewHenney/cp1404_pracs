5d9aede728ac900e2313de01ddd91454750578bf
AndrewHenney <andrew.henney@my.jcu.edu.au>
2017-08-31 09:37:13 +1000

    add colour_names

---
+++ prac_05/colour_names.py
@@ -0,0 +1,12 @@
+COLOUR_CODES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb",
+                "AntiqueWhite2": "#eedfcc", "AntiqueWhite3": "#cdc0b0", "AntiqueWhite4": "#8b8378",
+                "aquamarine1": "#7fffd4"}
+
+colour = input("Enter colour name: ")
+while colour != "":
+    if colour in COLOUR_CODES:
+        print("The hexadecimal code for", colour, "is", COLOUR_CODES[colour])
+    else:
+        print("Invalid colour name")
+    colour = input("Enter colour name: ")
+
