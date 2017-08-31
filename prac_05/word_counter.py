dcde33c0f098b0236097ade7774d7d07c83fd62c
AndrewHenney <andrew.henney@my.jcu.edu.au>
2017-08-31 10:31:25 +1000

    finished word_counter

--- prac_05/word_counter.py
+++ prac_05/word_counter.py
@@ -1,7 +1,14 @@
+
 input_string = input("Text: ")
-word_list = input_string.split( )
-word_count={}
+word_list = input_string.split()
+word_count = {}
 for word in word_list:
-    print(word)
     if word in word_count:
-        word_count[word]=
+        word_count[word] += 1
+    else:
+        word_count[word] = 1
+key_list = list(word_count.keys())
+key_list.sort()
+
+for word in key_list:
+    print('{} : {}'.format(word, word_count[word]))
