9ac3a754920fb3be1ed3013a80bc8a69b1b02f03
jc425395 <andrew.henney@my.jcu.edu.au>
2017-09-14 10:14:20 +1000

    changed name_labels to display labels

--- prac_06/name_labels.py
+++ prac_06/name_labels.py
@@ -1,6 +1,7 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.button import Button
+from kivy.uix.label import Label
 from kivy.properties import StringProperty
 
 
@@ -31,7 +32,7 @@ class NameLabelsApp(App):
         :return: None
         """
         for name in self.name_list:
-            temp_label = Button(text=name)
+            temp_label = Label(text = name)
             # add the button to the "entriesBox" using add_widget()
             self.root.ids.entriesBox.add_widget(temp_label)
 
