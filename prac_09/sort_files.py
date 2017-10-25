ef1ca2b58d51a07ab1dec4e5236a390c68ad10cf
Andrew Henney <30511670+AndrewHenney@users.noreply.github.com>
2017-10-25 23:41:54 +1000

    add file sort

--- prac_09/sort_files.py
+++ prac_09/sort_files.py
@@ -4,7 +4,19 @@ import os
 
 
 def main():
-    os.mkdir('FilesToSort')
+    extension_list = []
+    os.chdir('FilesToSort')
+    for filename in os.listdir('.'):
+        name, extension = filename.split('.')
+        print(extension)
+        if (extension in extension_list):
+            shutil.move(filename, extension)
+        else:
+            extension_list.append(extension)
+            os.mkdir(extension)
+            shutil.move(filename, extension)
+
+
 
 
 main()
